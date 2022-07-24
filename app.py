a4ab4ef13514ce33d3bb67c11a79597e5c1f0ad3
VidushAgarwal <59796609+VidushAgarwal@users.noreply.github.com>
2022-07-24 22:26:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Multiplication of 2 numbers')
+st.title('Divison of 2 numbers')
 a = st.number_input('Enter a number')
 b = st.number_input('Enter another number')
 if b!=0:
